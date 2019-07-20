39652b4c2dbd0614edf20ffafb8cd43911915621
Esther Kim <esthercodes2019@gmail.com>
2019-07-20 16:13:42 -0400

    Refactor: pulled out the game over conditions into another function

--- ticTacToe.js
+++ ticTacToe.js
@@ -7,31 +7,34 @@ const rl = readline.createInterface({ //rl is the CLI program that outputs in th
   output: process.stdout,
 })
 
+const gameOver = (winner) => {
+  if(!!game.checkForWin(board.multiplayer)){
+    console.log(`Player [${winner}] You Win!!!`)
+    return rl.close()
+  } else if (game.draw(board.multiplayer)){
+    console.log('It\'s a tie, you\'re both winners! Huzzah!')
+    return rl.close()
+  }
+}
+
 const gamePlay = () => {
   const token = game.currentPlayer(game.currentPlay)
 
   rl.question(`Player ${token}, your move:  `, (input) => {
     if(game.validatePlay(input, board.multiplayer)){ //checks to make sure you enter a number w/i range
-      // const token = game.currentPlayer(game.currentPlay)
       const newBoard = game.move(input, token) //updates the grid by putting player token at desired location
+      console.log(`\n`)
       board.displayBoard(newBoard) //displays the new grid on console.
       console.log(`\n`);
 
-      if(game.checkForWin(board.multiplayer)){
+      if((game.checkForWin(board.multiplayer)) || (game.draw(board.multiplayer))){
         const winner = game.currentPlayer(game.currentPlay)
-        console.log(`Player [${winner}] You Win!!!`)
-        rl.close()
+        gameOver(winner)
         return //without the return, the game continues to prompt
-      }
-      if (game.draw(board.multiplayer)){
-        console.log('It\'s a tie, you\'re both winners! Huzzah!')
-        rl.close()
-        return
       }
       game.currentPlay++
       gamePlay()
-
-    } else {
+    } else { //if the input is not valid
       console.log(`Invalid input, please try again \n`);
       gamePlay()
     }
