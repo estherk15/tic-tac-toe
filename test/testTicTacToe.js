4df903c8dde601b26e25622153a2fdb31932e883
estherk15 <esthercodes2019@gmail.com>
2019-08-19 09:56:18 -0400

    Cleaned up comments in tests

--- test/testTicTacToe.js
+++ test/testTicTacToe.js
@@ -20,34 +20,6 @@ describe('TicTacToe.js', () => {
       assert.equal(ticTacToe.startGame(outputTest, true), outputTest.read);
     });
   });
+  // given a fn how do you store a value that was passed into it
 
 })
-// Game play
-// describe('Board', () => {
-//   describe('#displayBoard()', () => {
-//     it('displays a board with numbers', () => {
-//       assert.deepStrictEqual(board.displayBoard(board.standard, true),
-//           '  1 | 2 | 3 \n -----------\n  4 | 5 | 6 \n -----------\n  7 | 8 | 9 ');
-//     });
-//   });
-// });
-// // ===========================================================================
-// validates that numbers are entered
-
-// describe('gameOver()', () => {
-//   it('Writes the winner to the console', () => {
-//     board.standard = ['X', 'X', 'X', 'O', 'O', 6, 7, 8, 9];
-//     assert.equal(ticTacToe.gameOver(''), true);
-//   });
-// });
-
-// describe('startGame()', () => {
-//   it('Writes the winner to the console', () => {
-//     const outputTest = () => {
-//       return {
-//         write: () => console.log('Welcome to the game'),
-//       };
-//     };
-//     expect(ticTacToe.startGame(outputTest)).to.equal('Welcome to the game');
-//   });
-// });
